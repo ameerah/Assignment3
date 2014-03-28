ce7d6b98fc136d1d01070ab17b6c3ffe4ffcd613
Ameerah <ameerah.allie@gmail.com>
2014-03-28 10:26:36 +0200

    added ass 1

---
+++ Assignment1/Assignment1/assignment1/task1.java
@@ -0,0 +1,21 @@
+//Assignment 1: Task 1
+//Using a sequential search to find mutual friends between text documents
+//9 March 2014
+//Ameerah Allie (ALLAME002)
+
+import java.util.Scanner;
+
+public class task1
+{
+	public static void main (String[] args)
+	{
+		
+		Scanner fileInput = new Scanner(new FileInputStream("toSearchFor.txt"));
+		
+		
+		
+		
+		
+	}
+	
+}
